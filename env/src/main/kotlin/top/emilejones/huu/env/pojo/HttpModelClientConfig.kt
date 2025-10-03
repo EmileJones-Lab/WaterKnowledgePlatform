cdbc7fa14f7d46b4125444de91d7df09f745a396
EmileJones <021027shf@gmail.com>
2025-10-03 20:41:32 +0800

    style(env): 修改model相关的配置文件的配置名称
    1.将模型配置名称从xinference变为model
    2.修改.gitignore，将config.yml和qa.csv列入忽略名单

--- env/src/main/kotlin/top/emilejones/huu/env/pojo/HttpModelClientConfig.kt
+++ env/src/main/kotlin/top/emilejones/huu/env/pojo/HttpModelClientConfig.kt
@@ -3,7 +3,7 @@ package top.emilejones.huu.env.pojo
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class XinferenceConfig(
+data class HttpModelClientConfig(
     val host: String,
     val port: Int,
     val token: String,
