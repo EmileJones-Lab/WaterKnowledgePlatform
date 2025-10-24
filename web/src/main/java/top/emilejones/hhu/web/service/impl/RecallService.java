a3bb00f0fb96a98ec245477aaf06a81633718b63
EmileJones <021027shf@gmail.com>
2025-10-24 19:12:01 +0800

    style(RecallService): 删除不必要的依赖

--- web/src/main/java/top/emilejones/hhu/web/service/impl/RecallService.java
+++ web/src/main/java/top/emilejones/hhu/web/service/impl/RecallService.java
@@ -11,7 +11,6 @@ import top.emilejones.hhu.web.entity.TextNode;
 import top.emilejones.hhu.web.repository.IMilvusRepository;
 import top.emilejones.hhu.web.repository.INeo4jRepository;
 import top.emilejones.hhu.web.service.IRecallService;
-import top.emilejones.hhu.web.service.strategy.ObtainWholeTableStrategy;
 import top.emilejones.huu.env.pojo.ApplicationConfig;
 
 import java.util.List;
@@ -27,14 +26,11 @@ public class RecallService implements IRecallService {
     private static Logger logger = LoggerFactory.getLogger(RecallService.class);
     private final ApplicationConfig config;
 
-    private final ObtainWholeTableStrategy obtainWholeTableStrategy;
-
     public RecallService(IMilvusRepository milvusRepository, INeo4jRepository neo4jRepository, ApplicationConfig config, ModelClient client) {
         this.milvusRepository = milvusRepository;
         this.neo4jRepository = neo4jRepository;
         this.config = config;
         this.client = client;
-        obtainWholeTableStrategy = new ObtainWholeTableStrategy(neo4jRepository);
     }
 
     @Override
@@ -58,9 +54,7 @@ public class RecallService implements IRecallService {
                 }).toList();
         // 将milvus数据转换为neo4j数据
         List<Pair<FileNode, TextNode>> rawData = rerankResult.stream().map(milvusDatum -> neo4jRepository.selectByElementId(milvusDatum.getElementId())).toList();
-        // 将每一个表格节点向上向下查找，如果有表格上下文则加入
-        List<Pair<FileNode, TextNode>> wholeTableData = obtainWholeTableStrategy.exec(rawData);
-        logger.info("用户问题为：[{}]，召回的节点数量为[{}]个", query, wholeTableData.size());
-        return wholeTableData;
+        logger.info("用户问题为：[{}]，召回的节点数量为[{}]个", query, rawData.size());
+        return rawData;
     }
 }
