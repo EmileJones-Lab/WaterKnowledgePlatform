f9e557a6dd20fb6049ba73a28aa67db848499c12
EmileJones <021027shf@gmail.com>
2025-11-06 16:44:43 +0800

    pref(recall): 增加日志输出

--- web/src/main/java/top/emilejones/hhu/web/service/impl/RecallService.java
+++ web/src/main/java/top/emilejones/hhu/web/service/impl/RecallService.java
@@ -5,8 +5,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import top.emilejones.hhu.model.ModelClient;
-import top.emilejones.hhu.web.entity.FileNode;
 import top.emilejones.hhu.web.entity.DenseRecallResult;
+import top.emilejones.hhu.web.entity.FileNode;
 import top.emilejones.hhu.web.entity.TextNode;
 import top.emilejones.hhu.web.repository.IMilvusRepository;
 import top.emilejones.hhu.web.repository.INeo4jRepository;
@@ -14,6 +14,7 @@ import top.emilejones.hhu.web.service.IRecallService;
 import top.emilejones.huu.env.pojo.ApplicationConfig;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author EmileJones
@@ -52,7 +53,16 @@ public class RecallService implements IRecallService {
                 .map(rr -> searchResults.get(rr.getIndex())).toList();
         // 将milvus数据转换为neo4j数据
         List<Pair<FileNode, TextNode>> rawData = rerankResult.stream().map(denseRecallResult -> neo4jRepository.selectByElementId(denseRecallResult.getElementId())).toList();
+        logger.debug("查询问题[{}]召回节点的cypher语句为[{}]", query, generateCypherByTextNodeList(rawData.stream().map(Pair::getSecond).toList()));
         logger.info("用户问题为：[{}]，召回的节点数量为[{}]个", query, rawData.size());
         return rawData;
+    }
+
+    private String generateCypherByTextNodeList(List<TextNode> textNodeList) {
+        String list = textNodeList.stream()
+                .map(TextNode::getElementId)
+                .map(it -> "\"" + it + "\"")
+                .collect(Collectors.joining(", ", "[", "]"));
+        return "MATCH (n: TextNode) WHERE elementId(n) IN %s MATCH (f:FileNode)-[:CONTAIN]->(n) RETURN n,f".formatted(list);
     }
 }
