323aba8d2c7e6aaf2db821b1691933661880cf2e
EmileJones <021027shf@gmail.com>
2025-11-17 21:21:21 +0800

    refactor(web): 将前端接口分离出一个VO层，同时将文档写的更加详细

--- controller-web/src/main/java/top/emilejones/hhu/web/controller/RecallController.java
+++ controller-web/src/main/java/top/emilejones/hhu/web/controller/RecallController.java
@@ -11,15 +11,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import top.emilejones.hhu.domain.po.Neo4jTextNode;
 import top.emilejones.hhu.service.IRecallService;
+import top.emilejones.hhu.web.vo.TextNodeVO;
 
 import java.util.List;
 
 
 @RestController
 @RequestMapping("/recall")
-@Tag(name = "retrieve", description = "关于召回文本的功能接口说明")
+@Tag(name = "retrieval", description = "关于召回文本的功能接口说明")
 public class RecallController {
 
     private final IRecallService recallService;
@@ -38,12 +38,15 @@ public class RecallController {
                     mediaType = "application/json",
                     array = @ArraySchema(
                             schema = @Schema(
-                                    implementation = String.class,
-                                    type = "array"
-                            ))
+                                    description = "文本片段"
+                            ),
+                            arraySchema = @Schema(
+                                    description = "文本片段列表"
+                            )
+                    )
             )
     )
-    public List<String> recallText(@RequestParam("query") String query) {
+    public List<String> recallText(@RequestParam("query") @Schema(description = "用户的问题") String query) {
         return recallService.recallText(query);
     }
 
@@ -52,18 +55,18 @@ public class RecallController {
             summary = "通过问题去召回相关片段，并且返回每一个片段的详细信息",
             description = "根据问题去图数据库中召回相关片段，并且基于已有策略去图数据库中查询上下文，最后返回每一个片段的详细信息"
     )
-    @ApiResponse(responseCode = "200", description = "成功召回文本",
-            content = @Content(
-                    mediaType = "application/json",
-                    array = @ArraySchema(
-                            schema = @Schema(
-                                    implementation = Neo4jTextNode.class,
-                                    type = "array"
-                            )
-                    )
-            )
-    )
-    public List<Neo4jTextNode> recallNode(@RequestParam("query") String query) {
-        return recallService.recallNode(query).stream().map(Pair::getSecond).toList();
+    @ApiResponse(responseCode = "200", description = "成功召回文本")
+    public List<TextNodeVO> recallNode(@RequestParam("query") @Schema(description = "用户的问题") String query) {
+        return recallService.recallNode(query).stream()
+                .map(Pair::getSecond)
+                .map(neo4jTextNode -> {
+                    TextNodeVO textNodeVO = new TextNodeVO();
+                    textNodeVO.setText(neo4jTextNode.getText());
+                    textNodeVO.setLevel(neo4jTextNode.getLevel());
+                    textNodeVO.setSeq(neo4jTextNode.getSeq());
+                    textNodeVO.setElementId(neo4jTextNode.getElementId());
+                    textNodeVO.setType(neo4jTextNode.getType());
+                    return textNodeVO;
+                }).toList();
     }
 }
---
+++ controller-web/src/main/java/top/emilejones/hhu/web/vo/TextNodeVO.java
@@ -0,0 +1,69 @@
+package top.emilejones.hhu.web.vo;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import top.emilejones.hhu.domain.enums.TextType;
+
+@Schema(description = "切片节点相关数据")
+public class TextNodeVO {
+    @Schema(description = "节点唯一ID")
+    private String elementId;
+    @Schema(description = "切片文本")
+    private String text;
+    @Schema(description = "切片在当前文件的序号")
+    private Integer seq;
+    @Schema(description = "此节点属于几级标题（如果是INT_MAX就是文本节点）")
+    private Integer level;
+    @Schema(description = "这段切片是什么类型的内容")
+    private TextType type;
+
+    public String getElementId() {
+        return elementId;
+    }
+
+    public void setElementId(String elementId) {
+        this.elementId = elementId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Integer getSeq() {
+        return seq;
+    }
+
+    public void setSeq(Integer seq) {
+        this.seq = seq;
+    }
+
+    public Integer getLevel() {
+        return level;
+    }
+
+    public void setLevel(Integer level) {
+        this.level = level;
+    }
+
+    public TextType getType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return "TextNodeVO{" +
+                "elementId='" + elementId + '\'' +
+                ", text='" + text + '\'' +
+                ", seq=" + seq +
+                ", level=" + level +
+                ", type=" + type +
+                '}';
+    }
+
+    public void setType(TextType type) {
+        this.type = type;
+    }
+}
