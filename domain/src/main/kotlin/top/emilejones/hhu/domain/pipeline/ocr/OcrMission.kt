863c97f5df31c39d74faa1bf5667cd77a312288b
EmileJones <021027shf@gmail.com>
2025-11-27 19:39:08 +0800

    refactor: 修改pipeline的domain设计

--- domain/src/main/kotlin/top/emilejones/hhu/domain/pipeline/ocr/OcrMission.kt
+++ domain/src/main/kotlin/top/emilejones/hhu/domain/pipeline/ocr/OcrMission.kt
@@ -40,24 +40,10 @@ class OcrMission(
     }
 
     /** 启动 OCR 任务 */
-    fun start(gateway: SourceDocumentGateway, ocrGateway: OcrGateway) {
+    fun start() {
         require(status == MissionStatus.PENDING) { "OCR can only start from PENDING state." }
         status = MissionStatus.RUNNING
         startTime = Instant.now()
-
-        val sourceDocument = gateway.getSourceDocument(sourceDocumentId)
-        if (sourceDocument == null) {
-            failure("源文件不存在！")
-            return
-        }
-        val result = runCatching { ocrGateway.startOcr(sourceDocument.inputStream) }
-        if (result.isFailure) {
-            val msg = result.exceptionOrNull()?.message ?: "未知的异常"
-            failure(msg)
-        }
-
-        val processedDocument = result.getOrThrow()
-        success(processedDocument)
     }
 
     fun isCompleted(): Boolean = status == MissionStatus.SUCCESS || status == MissionStatus.ERROR
@@ -65,7 +51,7 @@ class OcrMission(
     fun isSuccess(): Boolean = status == MissionStatus.SUCCESS
 
     /** 完成 OCR 任务，必须包含结果 */
-    private fun success(processedDocument: ProcessedDocument) {
+    fun success(processedDocument: ProcessedDocument) {
         require(status == MissionStatus.RUNNING) { "OCR can only complete from RUNNING state." }
         result = OcrMissionResult.Success(processedDocument)
         status = MissionStatus.SUCCESS
@@ -73,7 +59,7 @@ class OcrMission(
     }
 
     /** OCR 执行失败 */
-    private fun failure(reason: String) {
+    fun failure(reason: String) {
         require(status == MissionStatus.RUNNING) { "OCR can only fail from RUNNING state." }
         status = MissionStatus.ERROR
         result = OcrMissionResult.Failure(reason)
