114ab0d8f345ec7887ca155ccd1214c5d8d5eead
EmileJones <021027shf@gmail.com>
2026-04-02 10:44:45 +0800

    feat: 接入LLM用来修正文档结构
    1. 修改全局LLM的温度以及关闭深度思考模式
    2. 使用装饰器来将AI的部分和纯代码的部分解耦

--- infrastructure/textsplitter/structure-extraction/src/main/kotlin/top/emilejones/hhu/textsplitter/config/MarkdownStructureExtractorConfig.kt
+++ infrastructure/textsplitter/structure-extraction/src/main/kotlin/top/emilejones/hhu/textsplitter/config/MarkdownStructureExtractorConfig.kt
@@ -2,11 +2,14 @@ package top.emilejones.hhu.textsplitter.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import top.emilejones.hhu.model.ModelClient
 import top.emilejones.hhu.preprocessing.structure.MarkdownStructureExtractor
 import top.emilejones.hhu.preprocessing.structure.TitleTreeExtractor
+import top.emilejones.hhu.preprocessing.structure.TitleTreeExtractorWithAI
 
 @Configuration
 class MarkdownStructureExtractorConfig {
     @Bean
-    fun getMarkdownStructureExtractorConfig(): MarkdownStructureExtractor = TitleTreeExtractor()
+    fun getMarkdownStructureExtractorConfig(modelClient: ModelClient): MarkdownStructureExtractor =
+        TitleTreeExtractorWithAI(TitleTreeExtractor(), modelClient)
 }
